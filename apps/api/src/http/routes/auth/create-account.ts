93a5226d864cb3dd18f25ff60a851577f6391f71
Bruno Gallotte <brunogallotte1@gmail.com>
2024-06-02 17:15:06 -0300

    Feat: add authenticate route and jwt sign

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,17 +1,65 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod'
+import { prisma } from "@/lib/prisma";
 
 export async function createAccount(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/users', {
         schema: {
+            tags: ['auth'],
+            summary: 'Create a new account',
             body: z.object({
                 name: z.string(),
                 email: z.string(),
                 password: z.string().min(6)
-            })
+            }),
+            response: {
+                400: z.object({
+                    message: z.string()
+                }),
+                201: z.object({
+                    token: z.string()
+                })
+            }
         }
-    } ,() => {
-        return 'User created!'
+    } , async (request, reply) => {
+        const { name, email, password } = request.body
+
+        const userWithSameEmail = await prisma.user.findUnique({
+            where: {
+                email,
+            }
+        })
+
+        if (userWithSameEmail) {
+            return reply.status(400).send({ message: 'User with same e-mail already exists.' })
+        }
+
+        const [, domain] = email.split('@')
+
+        const autoJoinOrganization = await prisma.organization.findFirst({
+            where: {
+                domain,
+                shouldAttachUserByDomain: true,
+            }
+        })
+
+        const passwordHash = await hash(password, 6)
+
+        await prisma.user.create({
+            data: {
+                name,
+                email,
+                passwordHash,
+                member_on: autoJoinOrganization ? {
+                    create: {
+                        organizationId: autoJoinOrganization.id,
+                    },
+                } : undefined,
+            }
+        })
+
+        reply.status(201).send()
     })
 }
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,15 +1,42 @@
 import fastifyCors from '@fastify/cors'
 import { fastify } from 'fastify'
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Next.js SaaS',
+        description: 'Full-stack SaaS app with multi-tenant & RBAC.',
+        version: '1.0.0'
+      },
+      servers: [],
+    },
+    transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUI, {
+    routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+    secret: 'my-jwt-secret',
+})
 
 app.register(fastifyCors)
+
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP server running!'))
