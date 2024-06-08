c31bacce839792b9d9c7aea09536c2dd3445fcc4
Bruno Gallotte <brunogallotte1@gmail.com>
2024-06-08 10:13:56 -0300

    :zap: Add error handler

--- apps/api/src/http/routes/auth/authenticate-with-password.ts
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -3,6 +3,7 @@ import { compare } from "bcryptjs";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
+import { BadRequestError } from "../_errors/bad-request-error";
 
 export async function authenticateWithPassword(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/sessions/password', {
@@ -12,7 +13,12 @@ export async function authenticateWithPassword(app: FastifyInstance) {
             body: z.object({
                 email: z.string().email(),
                 password: z.string()
-            })
+            }),
+            response: {
+                201: z.object({
+                    token: z.string()
+                })
+            }
         }
     }, async (request, reply) => {
         const { email, password } = request.body
@@ -22,17 +28,17 @@ export async function authenticateWithPassword(app: FastifyInstance) {
         })
 
         if (!userFromEmail) {
-            return reply.status(400).send({ message: 'Invalid credentials.' })
+            throw new BadRequestError('Invalid credentials.')
         }
 
         if (userFromEmail.passwordHash === null) {
-            return reply.status(400).send({ message: 'User does not have a password, use social login.' })
+            throw new BadRequestError('User doest not have a password, use social login.')
         }
 
         const isPasswordValid = await compare(password, userFromEmail.passwordHash)
 
         if (!isPasswordValid) {
-            return reply.status(400).send({ message: 'Invalid credentials.' })
+            throw new BadRequestError('Invalid credentials.')
         }
 
         const token = await reply.jwtSign(
@@ -45,7 +51,7 @@ export async function authenticateWithPassword(app: FastifyInstance) {
             }
         })
 
-        return reply.status(201).send(token)
+        return reply.status(201).send({ token })
     })
 }
 
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -7,6 +7,7 @@ import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProv
 import { createAccount } from './routes/auth/create-account'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { getProfile } from './routes/auth/get-profile'
+import { errorHandler } from './error-handler'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -14,6 +15,8 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.setErrorHandler(errorHandler)
 
 app.register(fastifySwagger, {
     openapi: {
